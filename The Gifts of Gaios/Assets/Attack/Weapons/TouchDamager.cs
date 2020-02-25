cbe76e29605813f24817dcfe90d8b702e67f57c2
Jono Matthews <jono.jmm@gmail.com>
2020-02-24 19:18:20 -0600

    .

--- The Gifts of Gaios/Assets/Attack/Weapons/TouchDamager.cs
+++ The Gifts of Gaios/Assets/Attack/Weapons/TouchDamager.cs
@@ -12,7 +12,7 @@ public class TouchDamager : MonoBehaviour {
         assigner = GetComponent<TeamAssigner>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionStay2D(Collision2D collision) {
         TeamAssigner otherTeamAssigner = collision.collider.GetComponent<TeamAssigner>();
 
         if(otherTeamAssigner != null && otherTeamAssigner.team != assigner.team) {
